766941951432d8d92eaf13ef7898292306ce2484
Buddhi Edirisinghe <edirisinghe.buddhi@gmail.com>
2015-12-31 12:16:12 +0530

    chenge some output message

--- TestPatterns/src/org/buddhi/pattern/singleton/Singleton.java
+++ TestPatterns/src/org/buddhi/pattern/singleton/Singleton.java
@@ -1,13 +1,10 @@
 package org.buddhi.pattern.singleton;
 
- class Singleton {
+  class Singleton {
 
 	private static Singleton instance;
 
 
-	private  Singleton(){
-		
-	}
 
 	public static Singleton getInstance() {
 		if (instance == null) {
